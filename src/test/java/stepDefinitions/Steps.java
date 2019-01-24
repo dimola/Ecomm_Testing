c9b08580e2ba805d173aa13f3881ec8d2cc78d78
Dimo Lalushev <dlalushev@QH.NET>
2019-01-24 15:17:00 +0200

    Updated project structure

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,55 +1,60 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import dataProviders.ConfigFileReader;
+import managers.PageObjectManager;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import pageObjects.LoginPage;
 
 public class Steps {
-	
 	WebDriver driver;
-	
+	LoginPage login;
+	PageObjectManager pageObjectManager;
+	ConfigFileReader configFileReader;
 
 	@Given("^Login page is loaded$")
 	public void Login_page_is_loaded() throws Throwable {
-		System.setProperty("webdriver.chrome.driver","D:\\Java\\chromedriver_win32\\chromedriver.exe");
+		
+		configFileReader = new ConfigFileReader();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		 driver.get("http://acme.qualityhouse.bg/build3/index.php?page=login");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		pageObjectManager = new PageObjectManager(driver);
+		login = pageObjectManager.getLoginPage();
+		login.getLoginPage(driver);
 	}
 
 	@When("^I type valid username$")
 	public void I_type_valid_username() throws Throwable {
-	   WebElement username = driver.findElement(By.xpath("//*[@id=\"username\"]"));
-	   username.sendKeys("student1");
+		login = new LoginPage(driver);
+		String username = null;
+		login.enterUserName(username);
+			 
 	}
 
 	@And("^I type valid password$")
 	public void I_type_valid_password() throws Throwable {
-		WebElement password = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/form/input[2]"));
-		   password.sendKeys("stpass1");  
+		login = new LoginPage(driver);
+		String password = null;
+		login.enterPassword(password);
 	}
 
 	@And("^I click on Login button$")
 	public void I_click_on_Login_button() throws Throwable {
-	   WebElement SubmitButton = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/form/input[3]"));
-	   SubmitButton.click();
+		login = new LoginPage(driver);
+		login.clickLogIn();
 	}
 
 	@Then("^I should be successfully logged in$")
 	public void I_should_be_successfully_logged_in() throws Throwable {
 	
-		if(driver.findElements(By.xpath("/html/body/div[1]/div[1]/div[3]/a[6]")).size() != 0){
+		if(driver.findElements(By.xpath("//*[@id=\"main-menu\"]/a[6]")).size() != 0){
 			System.out.println("Logout button is Present and user is successfully logged in");
 			}else{
 			System.out.println("Login button is Absent and user is not logged in ");
@@ -60,6 +65,8 @@ public class Steps {
 		}
 	
 	}
+
+
 
 
 
