dc8250601d6093e7bc04fff14e988ee85ecf07c3
Yakim Rachev <jakimcho@gmail.com>
2019-02-06 17:06:47 +0200

    Merge with master and fixed project pom

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,9 +5,14 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/functionalTests", glue = "stepDefinitions")
 
-public class TestRunner {
+@RunWith( Cucumber.class )
+@CucumberOptions(
+        features = "features",
+        glue = { "stepDefinitions" }
+)
+
+public class TestRunner
+{
 
 }
