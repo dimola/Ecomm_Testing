474b89f27497573093041da7274eb9c4c12e899a
Yoana Nikolova <ynikolova@qualityhouse.com>
2019-04-02 09:24:20 +0300

    Rewriting steps with custom assertions

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -7,6 +7,10 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import managers.PageObjectManager;
+import pageObjects.BooksPage;
+import pageObjects.HomePage;
+
+import static customAssertions.CustomAssertions.assertThat;
 
 public class Steps {
 
@@ -19,6 +23,13 @@ public class Steps {
 
 	@Then("^I should be successfully logged in$")
 	public void I_should_be_successfully_logged_in() throws Throwable {
+		assertThat(pageObjectManager).homePageIsOpen();
+		HomePage homePage = pageObjectManager.getHomePage();
+
+		assertThat(homePage.getLoginMenuElement()).isNotDisplayed();
+		assertThat(homePage.getRegisterMenuElement()).isNotDisplayed();
+		assertThat(homePage.getLogoutMenuElement()).isDisplayed();
+		/*
 		Assert.assertTrue("Home page is not on focus", pageObjectManager.getHomePage().isOpen());
 		Assert.assertFalse("Login button is displayed after login but it should not",
 				pageObjectManager.getHomePage().isLoginButtonDisplayed());
@@ -26,16 +37,19 @@ public class Steps {
 				pageObjectManager.getHomePage().isRegisterButtonDisplayed());
 		Assert.assertTrue("Logout button is not displayed after login but it should",
 				pageObjectManager.getHomePage().isLogoutButtonDisplayed());
+				*/
 	}
 
 	@Then("^I am not logged in the system$")
 	public void I_am_not_logged_in_the_system() throws Throwable {
-		Assert.assertTrue("Home page is not on focus", pageObjectManager.getLoginPage().isOpen());
+		assertThat(pageObjectManager).loginPageIsOpen();
+		//Assert.assertTrue("Home page is not on focus", pageObjectManager.getLoginPage().isOpen());
 	}
 
 	@Then("^An error message is displayed$")
 	public void An_error_message_is_displayed() throws Throwable {
-		Assert.assertTrue("Timer is not displayed", pageObjectManager.getLoginPage().timerIsDisplayed());
+		assertThat(pageObjectManager.getLoginPage().getTimerMessage()).isDisplayed();
+		//Assert.assertTrue("Timer is not displayed", pageObjectManager.getLoginPage().timerIsDisplayed());
 	}
 
 	@Given("^I am logged in with credentials \"([^\"]*)\" and \"([^\"]*)\"$")
@@ -52,12 +66,18 @@ public class Steps {
 
 	@Then("^I am successfully logged out$")
 	public void I_am_successfully_logged_out() throws Throwable {
+		HomePage homePage = pageObjectManager.getHomePage();
+		assertThat(homePage.getLoginMenuElement()).isDisplayed();
+		assertThat(homePage.getRegisterMenuElement()).isDisplayed();
+		assertThat(homePage.getLogoutMenuElement()).isNotDisplayed();
+		/*
 		Assert.assertTrue("Login button is not displayed after logout",
 				pageObjectManager.getHomePage().isLoginButtonDisplayed());
 		Assert.assertTrue("Register button is not displayed after logout",
 				pageObjectManager.getHomePage().isRegisterButtonDisplayed());
 		Assert.assertFalse("Logout button is displayed after logout",
 				pageObjectManager.getHomePage().isLogoutButtonDisplayed());
+				*/
 	}
 
 	// TC 20 Verify that links are redirecting to correct place
@@ -78,11 +98,17 @@ public class Steps {
 	@Then("^I am redirected to the respective category \"([^\"]*)\"$")
 	public void I_am_redirected_to_the_respective_category(String page) throws Throwable {
 		if (page == "BooksPage") {
-			Assert.assertTrue("Problems while verifying that Books Category Page is displayed",
+			assertThat(pageObjectManager).booksPageIsOpen();
+			/*Assert.assertTrue("Problems while verifying that Books Category Page is displayed",
 					pageObjectManager.getBooksPage().isOpen());
-		} else
-			Assert.assertTrue("Problems while verifying that Cds Category Page is displayed",
+					*/
+		} else{
+			assertThat(pageObjectManager).cdsPageIsOpen();
+			/*Assert.assertTrue("Problems while verifying that Cds Category Page is displayed",
 					pageObjectManager.getCdsPage().isOpen());
+					*/
+		}
+
 	}
 
 	@When("^I redirect to books page$")
@@ -92,6 +118,21 @@ public class Steps {
 
 	@Then("^I should see the books page$")
 	public void i_should_see_the_books_page() throws Throwable {
+		BooksPage booksPage = pageObjectManager.getBooksPage();
+
+		assertThat(booksPage.getLogo()).isDisplayed();
+		assertThat(booksPage.getMainMenuElement()).isDisplayed();
+		assertThat(booksPage.getHomeMenuElement()).isDisplayed();
+		assertThat(booksPage.getBooksMenuElement()).isDisplayed();
+		assertThat(booksPage.getCdsMenuElement()).isDisplayed();
+		assertThat(booksPage.getBasketMenuElement()).isDisplayed();
+		assertThat(booksPage.getRegisterMenuElement()).isDisplayed();
+		assertThat(booksPage.getLoginMenuElement()).isDisplayed();
+		assertThat(booksPage.getViewBasketMenuElement()).isDisplayed();
+		assertThat(booksPage.getSideMenu()).isDisplayed();
+		assertThat(booksPage.getProductList()).isDisplayed();
+		assertThat(booksPage.getSearchForm()).isDisplayed();
+		/*
 		Assert.assertTrue("Logo is not displayed on the page",pageObjectManager.getBooksPage().isLogoDisplayed());
 		Assert.assertTrue("Main Menu is not displayed on the page",pageObjectManager.getBooksPage().isMainMenuDisplayed());
 		Assert.assertTrue("Home button in the Main Menu is not displayed on the page",pageObjectManager.getBooksPage().isHomeButtonDisplayed());
@@ -104,14 +145,23 @@ public class Steps {
 		Assert.assertTrue("Side Menu is not displayed on the page",pageObjectManager.getBooksPage().isSideMenuDisplayed());
 		Assert.assertTrue("The Books Product list is not displayed on the page",pageObjectManager.getBooksPage().isProductListDisplayed());
 		Assert.assertTrue("Search Form is not displayed on the page",pageObjectManager.getBooksPage().isSearchFormDisplayed());
+		*/
 	}
 
 	@Then("^I should see all book filtering options$")
 	public void i_should_see_all_book_filtering_options() throws Throwable {
+		BooksPage booksPage = pageObjectManager.getBooksPage();
+
+		assertThat(booksPage.getAuthorTextbox()).isDisplayed();
+		assertThat(booksPage.getTitleTextbox()).isDisplayed();
+		assertThat(booksPage.getPublisherTextbox()).isDisplayed();
+		assertThat(booksPage.getIsbnTextbox()).isDisplayed();
+		/*
 		Assert.assertTrue("Author textbox in the Search Form is not displayed on the page",pageObjectManager.getBooksPage().isAuthorTextboxDisplayed());
 		Assert.assertTrue("Title textbox in the Search Form is not displayed on the page",pageObjectManager.getBooksPage().isTitleTextboxDisplayed());
 		Assert.assertTrue("Publisher textbox in the Search Form is not displayed on the page",pageObjectManager.getBooksPage().isPublisherTextboxDisplayed());
-		Assert.assertTrue("ISBN textbox in the Search Form is not displayed on the page",pageObjectManager.getBooksPage().isIsbnTextboxDisplayed());   
+		Assert.assertTrue("ISBN textbox in the Search Form is not displayed on the page",pageObjectManager.getBooksPage().isIsbnTextboxDisplayed());
+		*/
 	}
 	
 	@Before
