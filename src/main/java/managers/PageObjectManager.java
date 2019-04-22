e7430a53634dff4ecd30b4067861b6dc048e6d5f
Yoana Nikolova <ynikolova@qualityhouse.com>
2019-04-22 09:04:17 +0300

    Removed unnecessary lines

--- src/main/java/managers/PageObjectManager.java
+++ src/main/java/managers/PageObjectManager.java
@@ -1,11 +1,8 @@
 package managers;
 
 import java.util.concurrent.TimeUnit;
-
-import com.sun.deploy.util.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import dataProviders.ConfigFileReader;
 import pageObjects.*;
 
